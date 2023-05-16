00adbca4082cf97a519694c64222c6f764fd3d81
Braunerr <mathn519@gmail.com>
2023-05-16 10:00:44 +0200

    removed usings

--- BidService/Controllers/BidServiceController.cs
+++ BidService/Controllers/BidServiceController.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace BidService.Controllers;
 
@@ -13,27 +8,10 @@ namespace BidService.Controllers;
 [Route("[controller]")]
 public class BidServiceController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    private readonly ILogger<BidServiceController> _logger;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public BidServiceController(ILogger<BidServicesController> logger)
+    public BidServiceController(ILogger<BidServiceController> logger)
     {
         _logger = logger;
-    }
-
-    [HttpGet(Name = "GetBidService")]
-    public IEnumerable<BidServices> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new BidServices
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
     }
 }
