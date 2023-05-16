9830fcd894b2505fdd8a4494323c14727eb97070
NicklasAbrahamsen <95019758+NicklasAbrahamsen@users.noreply.github.com>
2023-05-16 12:13:03 +0200

    bid

--- BidService/Controllers/BidServiceController.cs
+++ BidService/Controllers/BidServiceController.cs
@@ -1,34 +1,94 @@
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
 using System.Text;
-using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using MongoDB.Driver;
 
-namespace BidService.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class BidServiceController : ControllerBase
+namespace BidService.Controllers
 {
-    private static readonly string[] Summaries = new[]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BidController : ControllerBase
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private readonly ILogger<BidController> _logger;
+        private readonly string _hostName;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public BidServiceController(ILogger<BidServicesController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetBidService")]
-    public IEnumerable<BidServices> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new BidServices
+        public BidController(ILogger<BidController> logger, IConfiguration config)
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            _logger = logger;
+            _hostName = config["HostnameRabbit"];
+            _logger.LogInformation($"Connection: {_hostName}");
+        }
+
+        // Placeholder for the auction data storage
+        private static readonly List<Auction> Auctions = new List<Auction>();
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAuction(Guid id)
+        {
+            MongoClient dbClient = new MongoClient(
+                "mongodb+srv://GroenOlsen:BhvQmiihJWiurl2V@auktionshusgo.yzctdhc.mongodb.net/?retryWrites=true&w=majority"
+            );
+            var collection = dbClient.GetDatabase("auction").GetCollection<Auction>("auctions");
+            Auction auction = await collection.Find(a => a.Id == id).FirstOrDefaultAsync();
+
+            if (auction == null)
+            {
+                return NotFound($"Auction with Id {id} not found.");
+            }
+            return Ok(auction);
+        }
+
+        [HttpPost("{id}/placeBid")]
+        public async Task<IActionResult> PlaceBid(Guid id, [FromBody] Bid bid)
+        {
+            MongoClient dbClient = new MongoClient(
+                "mongodb+srv://GroenOlsen:BhvQmiihJWiurl2V@auktionshusgo.yzctdhc.mongodb.net/?retryWrites=true&w=majority"
+            );
+            var collection = dbClient.GetDatabase("auction").GetCollection<Auction>("auctions");
+            var bidCollection = dbClient.GetDatabase("Bid").GetCollection<Bid>("Bids");
+
+            Auction auction = await collection.Find(a => a.Id == id).FirstOrDefaultAsync();
+
+            if (auction == null)
+            {
+                return NotFound($"Auction with Id {id} not found.");
+            }
+
+            if (auction.BidHistory == null)
+            {
+                auction.BidHistory = new List<Bid>();
+            }
+
+            if (bid.Amount <= auction.CurrentPrice)
+            {
+                return BadRequest(
+                    $"Bid amount must be higher than {auction.CurrentPrice} the current price."
+                );
+            }
+
+            bid.Id = Guid.NewGuid();
+            bid.Date = DateTime.UtcNow;
+            auction.BidHistory.Add(bid);
+            auction.CurrentPrice = bid.Amount;
+
+            var update = Builders<Auction>.Update
+                .Set(a => a.CurrentPrice, bid.Amount)
+                .Push(a => a.BidHistory, bid);
+
+            await collection.UpdateOneAsync(a => a.Id == id, update);
+
+            await bidCollection.InsertOneAsync(bid);
+
+            return CreatedAtAction(nameof(GetAuction), new { id = id }, auction);
+        }
+
     }
 }
